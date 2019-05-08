dfd6a0fe8c3ff63d45db20c1b2c349b875de6173
Alessandro Senese <senesealessandro@gmail.com>
2019-05-08 10:29:38 +0100

    Fix parcel configuration

--- wp-content/plugins/stampa/dist/index.js
+++ wp-content/plugins/stampa/dist/index.js
@@ -25597,7 +25597,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44217" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39973" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
