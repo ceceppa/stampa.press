d2741eb6f98d78081c133227aff61e9474c89853
Alessandro Senese <senesealessandro@gmail.com>
2019-06-10 09:38:07 +0100

    UI fixes

--- wp-content/plugins/stampa/dist/index.js
+++ wp-content/plugins/stampa/dist/index.js
@@ -28915,7 +28915,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44177" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35431" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
